020f1ac58857ac31197491e6ff4fcd377ad26327
Maximilian A. Giraldo <maxgiraldo@hey.com>
2022-08-03 08:51:54 -0700

    add best friend

--- Buggy/User.swift
+++ Buggy/User.swift
@@ -10,12 +10,20 @@ import Foundation
 class User {
     let name: String
     var friends = [User]()
+    var bestFriend: User!
 
     init(_ name: String) {
         self.name = name
     }
 
-    func addFriend(friend: User) {
+    deinit {
+        print("Deinit \(name)")
+    }
+
+    func addFriend(friend: User, makeBestFriend: Bool) {
+        if makeBestFriend {
+            bestFriend = friend
+        }
         friends.append(friend)
     }
 }
--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var users: NSMutableArray?
+    var currentUser: User = User("Current User")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +26,15 @@ class ViewController: UIViewController {
         for user in newUsers {
             u = user as! User
             print("User: \(u.name)")
+            if let bestFriend = u.bestFriend {
+                print("User best friend: \(bestFriend.name)")
+            }
         }
 
         users = newUsers
     }
 
     @IBAction func deleteUsers(_ sender: UIButton) {
-        print("Method: \(#function) in file: \(#file) line: \(#line) called.")
-
         deleteUsers(users: users!)
     }
 
@@ -40,8 +42,10 @@ class ViewController: UIViewController {
         let array = NSMutableArray()
         var previousUser: User = User("default")
         for i in 0..<10 {
+            let diceRoll = Int.random(in: 0 ... 1)
             let user = User("Person \(i)")
-            user.addFriend(friend: previousUser)
+            currentUser.addFriend(friend: user, makeBestFriend: true)
+            user.addFriend(friend: previousUser, makeBestFriend: diceRoll == 0 ? true : false)
             previousUser = user
             array.insert(user, at: i)
         }
@@ -50,6 +54,7 @@ class ViewController: UIViewController {
     }
 
     func deleteUsers(users: NSMutableArray) {
+        print(#function)
         for _ in 0...10 {
             users.removeObject(at: 0)
         }
